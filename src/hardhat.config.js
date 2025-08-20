18e5ed67bbcfddd947f3f9dea272e7f0e3a73278
Rohith_09 <rohithchandran0011@gmail.com>
2025-08-20 20:07:43 +0530

    Update hardhat.config.js

--- src/hardhat.config.js
+++ src/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: "0.8.19",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545",
-      chainId: 1337
+      url: "",
+      chainId:
     }
   },
   paths: {
